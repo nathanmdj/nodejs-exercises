bc61ffd10cc21dade4e4fd0b92b92b517e8f8925
nathanmdj <nathanmdj628@gmail.com>
2023-12-16 23:04:13 +0800

    first commit

---
+++ 01-intro.js
@@ -0,0 +1,12 @@
+const amount = 7;
+
+if (amount < 10) {
+    console.log('small number')
+}
+else {
+    console.log('large number');
+}
+
+
+console.log('my first node app!!!');
+
---
+++ 03-modules.js
@@ -0,0 +1,9 @@
+// modules 
+
+const name = require('./04-names');
+const sayHi = require('./05-utils');
+const data = require('./06-alternative-flavor');
+console.log(data);
+sayHi('Susan');
+sayHi(name.john);
+sayHi(name.peter);s
