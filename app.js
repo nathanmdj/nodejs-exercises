bbdf63bcafaf9b1a47578dc054379ee366922f27
nathanmdj <nathanmdj628@gmail.com>
2023-12-17 15:30:39 +0800

    added 2 files

---
+++ app.js
@@ -0,0 +1,7 @@
+const {createReadStream} = require('fs');
+
+const stream = createReadStream('../content/big.txt', {highWaterMark: 90000})
+
+stream.on('data', (result) => {
+    console.log(result)
+})
